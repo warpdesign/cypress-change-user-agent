c1164adf803c6f22b44ddddbf5292aa95865ace8
Nicolas Ramz <nicolas.ramz@adevinta.com>
2020-12-11 11:42:19 +0100

    ADDED: headers override test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  it('should configure visit headers', () => {
+    cy.visit('https://example.cypress.io', {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        'My-Header': 'foo'
+      }
+    })
   })
 })
